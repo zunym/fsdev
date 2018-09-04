d5f4d7a6b3b3fbc77a006a8676553a4dd74e752c
zunym <zunyinmon@zunyinmon.com>
2018-09-05 00:21:48 +0800

    day1 workshop

--- day1/my-day1-apps/README.md
+++ day1/my-day1-apps/README.md
@@ -1,4 +1,4 @@
-# MyDay1App
+# MyDay1Apps
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.5.
 
--- day1/my-day1-apps/src/app/app.component.spec.ts
+++ day1/my-day1-apps/src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'my-day1-app'`, async(() => {
+  it(`should have as title 'my-day1-apps'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-day1-app');
+    expect(app.title).toEqual('my-day1-apps');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-day1-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-day1-apps!');
   }));
 });
